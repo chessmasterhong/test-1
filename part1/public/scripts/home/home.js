daed6e41fd1290849d59cf43f291b18046408b52
Kevin Chan <chessmasterhong@yahoo.com>
2018-05-05 22:03:09 -0400

    Added sorting by name, alphabetical, and priority.

--- part1/public/scripts/home/home.js
+++ part1/public/scripts/home/home.js
@@ -1,6 +1,26 @@
 app.controller('homeController', function homeController($scope, $http) { // eslint-disable-line prefer-arrow-callback
   $scope.users = [];
 
+  // Sort configuration
+  $scope.sorts = [{
+    id: 'featured',
+    label: 'Featured',
+    orderBy: '',
+  }, {
+    id: 'alphabetical',
+    label: 'A-Z',
+    orderBy: 'name',
+  }, {
+    id: 'priority',
+    label: 'Priority',
+    orderBy: 'priority',
+  }];
+
+  // Set up default selected options
+  $scope.selected = {
+    sort: $scope.sorts[0],
+  };
+
   // Get users data
   $http.get('data/users.json')
     .then((res) => {
@@ -8,7 +28,6 @@ app.controller('homeController', function homeController($scope, $http) { // esl
       if (!res || !res.data || !res.data.data) {
         $scope.users = [];
       }
-
       $scope.users = res.data.data;
     })
     .catch(() => {
