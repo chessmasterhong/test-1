e20835c3c04e007dc96a6e9f02779cf05b41d2fc
Kevin Chan <chessmasterhong@yahoo.com>
2018-05-05 22:43:08 -0400

    Added filter by category.

--- part1/public/scripts/home/home.js
+++ part1/public/scripts/home/home.js
@@ -1,5 +1,6 @@
 app.controller('homeController', function homeController($scope, $http) { // eslint-disable-line prefer-arrow-callback
   $scope.users = [];
+  $scope.categories = [];
 
   // Sort configuration
   $scope.sorts = [{
@@ -19,7 +20,27 @@ app.controller('homeController', function homeController($scope, $http) { // esl
   // Set up default selected options
   $scope.selected = {
     sort: $scope.sorts[0],
+    category: '',
   };
+
+  /**
+   * Generates distinct list of categories based on users data.
+   * @param  {Object[]} data User data array.
+   * @param  {String} category User category.
+   * @return {String[]} Distinct list of categories.
+   */
+  function generateCategories(data) {
+    const results = [];
+    if (!Array.isArray(data)) {
+      return results;
+    }
+    data.forEach((d) => {
+      if (results.indexOf(d.category) < 0) {
+        results.push(d.category);
+      }
+    });
+    return results;
+  }
 
   // Get users data
   $http.get('data/users.json')
@@ -27,8 +48,10 @@ app.controller('homeController', function homeController($scope, $http) { // esl
       // Unexpected format
       if (!res || !res.data || !res.data.data) {
         $scope.users = [];
+        $scope.categories = [];
       }
       $scope.users = res.data.data;
+      $scope.categories = generateCategories($scope.users);
     })
     .catch(() => {
       $scope.users = [];
