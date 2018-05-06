15374bca077810f546f68da268c60bb4e325facc
Kevin Chan <chessmasterhong@yahoo.com>
2018-05-05 20:10:16 -0400

    Read users data from JSON. Updated grid display to show users data.

--- part1/public/scripts/home/home.js
+++ part1/public/scripts/home/home.js
@@ -1,3 +1,17 @@
-app.controller('homeController', function homeController($scope) {
-  $scope.users = [1, 2, 3, 4, 5, 6];
+app.controller('homeController', function homeController($scope, $http) { // eslint-disable-line prefer-arrow-callback
+  $scope.users = [];
+
+  // Get users data
+  $http.get('data/users.json')
+    .then((res) => {
+      // Unexpected format
+      if (!res || !res.data || !res.data.data) {
+        $scope.users = [];
+      }
+
+      $scope.users = res.data.data;
+    })
+    .catch(() => {
+      $scope.users = [];
+    });
 });
